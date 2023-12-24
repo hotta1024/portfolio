b6ddc38b97a54b67d6fa51bfb2f4c24e2e9bfadf
Rikuto Hotta <132864370+hotta1024@users.noreply.github.com>
2023-12-24 23:47:14 +0900

    Update script.js

--- otohime/script.js
+++ otohime/script.js
@@ -9,8 +9,8 @@ play.addEventListener("click", ()=> {
     music.play();
     music.volume = 0;
     var start_func = setInterval(function() {
-        music.volume = music.volume + (baseVol / 500);
-        if(music.volume >= baseVol - (baseVol / 500)) {
+        music.volume = music.volume + (baseVol / 800);
+        if(music.volume >= baseVol - (baseVol / 800)) {
             music.volume = baseVol;
             clearInterval(start_func);
         }
@@ -18,12 +18,14 @@ play.addEventListener("click", ()=> {
 }, false);
 
 pause.addEventListener("click", ()=> {
-    var end_func = setInterval(function() {
-        music.volume = music.volume - (baseVol / 100);
-        if(music.volume <= (baseVol / 100)) {
-            music.volume = baseVol;
-            music.pause();
-            clearInterval(end_func);
-        }
-    }, fadeSpeed * baseVol / 100);
-}, false);
+//     var end_func = setInterval(function() {
+//         music.volume = music.volume - (baseVol / 100);
+//         if(music.volume <= (baseVol / 100)) {
+//             music.volume = baseVol;
+//             music.pause();
+//             clearInterval(end_func);
+//         }
+//     }, fadeSpeed * baseVol / 100);
+// }, false
+    music.pause();
+});
